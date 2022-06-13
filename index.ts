848847b92f350b7b4efdbf317ad8891e683e0910
snyh <snyh@snyh.org>
2022-06-13 15:38:01 +0800

    enable withCredentials

--- index.ts
+++ index.ts
@@ -2,11 +2,17 @@ import { GrpcWebImpl } from "./internal/grpcweb"
 
 import { DevicesClientImpl, Devices } from "./devices/devices"
 
+import { grpc } from "@improbable-eng/grpc-web";
+
 export class lzcAPIGateway {
     constructor(host: string) {
-        const rpc = new GrpcWebImpl(host, {})
+        const myt = grpc.CrossBrowserHttpTransport({ withCredentials: true });
+
+        const rpc = new GrpcWebImpl(host, {
+            debug: true,
+            transport: myt,
+        })
         this.devices = new DevicesClientImpl(rpc);
     }
-
     public devices: Devices;
 }
