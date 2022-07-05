4723e3d1cf8ed04b248bb3ad05c66979a37667cf
snyh <snyh@snyh.org>
2022-07-05 10:07:57 +0800

    use docker to generate stable code

--- tools/gen.sh
+++ tools/gen.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-protoc -I . \
-       --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative \
-       --ts_proto_opt=outputClientImpl=grpc-web --ts_proto_opt=exportCommonSymbols=false --ts_proto_opt=esModuleInterop=true \
-       --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=.\
-       --plugin=/usr/bin/protoc-gen-grpc-java \
-       --java_out=./.cache/java \
-       --grpc-java_out=./.cache/java \
-       sys/*/*.proto \
-       localdevice/*/*.proto \
-       */*.proto
+set -e
 
-# jar -c -f cloud.lazycat.apis.jar -C ./.cache/java/ .
-# pushd gohelper/
-# go build
-# popd
+OUT_GO=$(pwd)/lang/go/
+OUT_JS=$(pwd)/lang/js/
+OUT_JAVA=$(pwd)/lang/java/lib/src/main/java/
+
+JS_PLUGIN_PATH=$(pwd)/lang/js/node_modules/.bin/protoc-gen-ts_proto
+
+mkdir -p $OUT_GO $OUT_JS $OUT_JAVA
+
+pushd protos
+
+protoc -I . \
+       --go_out=$OUT_GO\
+       --go-grpc_out=$OUT_GO\
+       --ts_proto_out=$OUT_JS\
+       --java_out=$OUT_JAVA \
+       --grpc-java_out=$OUT_JAVA \
+       --go_opt=paths=source_relative \
+       --go-grpc_opt=paths=source_relative \
+       --ts_proto_opt=outputClientImpl=grpc-web \
+       --ts_proto_opt=exportCommonSymbols=false \
+       --ts_proto_opt=esModuleInterop=true \
+       --plugin=$JS_PLUGIN_PATH \
+       --plugin=/usr/bin/protoc-gen-grpc-java \
+       ./sys/*/*.proto \
+       ./localdevice/*/*.proto \
+       ./common/*/*.proto
+popd
