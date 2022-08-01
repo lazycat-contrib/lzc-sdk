d5c7fbfd73e9190b6125a872d13bb1c1e9445cf8
snyh <snyh@snyh.org>
2022-08-01 21:11:45 +0800

    lang/go: fix import path

--- scontext/scontext.rules.go
+++ scontext/scontext.rules.go
@@ -4,11 +4,11 @@ package scontext
 
 func ListPermission(methodName string) []int {
 	switch methodName {
-	case "cloud.lazycat.apis.common.PeripheralDeviceService.MountFilesystem":
-		//[USER_DOCUMENT]
-		return []int{0}
 	case "cloud.lazycat.apis.sys.PackageManager.Install":
 		//[INSTALL_PACKAGE]
 		return []int{1}
+	case "cloud.lazycat.apis.common.PeripheralDeviceService.MountFilesystem":
+		//[USER_DOCUMENT]
+		return []int{0}
 	}
 }
