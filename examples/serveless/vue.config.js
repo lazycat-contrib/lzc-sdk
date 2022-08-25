a145efda7689c0eb107385d1aca4274e83d37221
snyh <snyh@snyh.org>
2022-08-25 16:21:55 +0800

    example/serverless: 开启hmr

--- examples/serveless/vue.config.js
+++ examples/serveless/vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   devServer: {
     historyApiFallback: true,
+    disableHostCheck: true,
+    //public: "serverless.snyht4.heiyu.space", //盒子里对应app的实际域名
   },
   chainWebpack: config => config.resolve.symlinks(false)
 };
