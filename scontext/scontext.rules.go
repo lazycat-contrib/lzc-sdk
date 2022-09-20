5840893c5ee7bd966447488710791630c88436aa
song <meetsong@foxmail.com>
2022-09-20 16:37:42 +0800

    增加编译file handler文件

--- scontext/scontext.rules.go
+++ scontext/scontext.rules.go
@@ -4,13 +4,13 @@ package scontext
 
 func ListPermission(methodName string) []int {
 	switch methodName {
-	case "cloud.lazycat.apis.sys.PackageManager.Uninstall":
-		//[INSTALL_PACKAGE]
-		return []int{1}
 	case "cloud.lazycat.apis.common.PeripheralDeviceService.MountFilesystem":
 		//[USER_DOCUMENT]
 		return []int{0}
 	case "cloud.lazycat.apis.sys.PackageManager.Install":
+		//[INSTALL_PACKAGE]
+		return []int{1}
+	case "cloud.lazycat.apis.sys.PackageManager.Uninstall":
 		//[INSTALL_PACKAGE]
 		return []int{1}
 	}
