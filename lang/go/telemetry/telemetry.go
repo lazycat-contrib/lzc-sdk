043c817c02a268f01547f385ccf49eb20f0bcd72
snyh <snyh@snyh.org>
2022-10-15 08:27:47 +0800

    lang/go: test telemetry code

---
+++ lang/go/telemetry/telemetry.go
@@ -0,0 +1,52 @@
+package telemetry
+
+import (
+	"context"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	sdkresource "go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"google.golang.org/grpc"
+)
+
+func InitTracer(ctx context.Context) error {
+	exporter, err := otlptrace.New(
+		ctx,
+		otlptracegrpc.NewClient(
+			otlptracegrpc.WithInsecure(),
+			otlptracegrpc.WithDialOption(
+				grpc.WithBlock(),
+			),
+			otlptracegrpc.WithEndpoint("host.lzcapp:4317"),
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	res, err := sdkresource.New(ctx,
+		sdkresource.WithAttributes(
+			semconv.ServiceNameKey.String(os.Getenv("LAZYCAT_APP_ID")),
+		),
+	)
+	if err != nil {
+		return err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithSyncer(exporter),
+		//		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(res),
+	)
+
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	otel.SetTracerProvider(tp)
+
+	return nil
+}
