08e170a197a053dcc4cbd115c5206be6ddb5788e
snyh <snyh@snyh.org>
2022-10-15 10:56:49 +0800

    telemetry/go: support Tracer

--- lang/go/telemetry/telemetry.go
+++ lang/go/telemetry/telemetry.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 	"os"
+	"sync"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
@@ -11,6 +12,8 @@ import (
 	"go.opentelemetry.io/otel/propagation"
 	sdkresource "go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"google.golang.org/grpc"
 )
@@ -60,7 +63,42 @@ func apply(opts ...Option) (*_Config, error) {
 	return cfg, nil
 }
 
-func InitTracer(ctx context.Context, opts ...Option) error {
+// 等待InitTracer调用后替换实际的tracer
+type lazyTracer struct {
+	real trace.Tracer
+	lock sync.Mutex
+
+	name string
+	opts []trace.TracerOption
+}
+
+func (l *lazyTracer) Start(ctx context.Context, spanName string, opts ...trace.SpanStartOption) (context.Context, trace.Span) {
+	if tp == nil {
+		panic("Use Tracer before InitTracer")
+	}
+
+	l.lock.Lock()
+	if l.real == nil {
+		l.real = tp.Tracer(l.name, l.opts...)
+	}
+	l.lock.Unlock()
+
+	return l.real.Start(ctx, spanName, opts...)
+}
+
+var (
+	tp   *sdktrace.TracerProvider
+	lock sync.RWMutex
+)
+
+func Tracer(name string, opts ...trace.TracerOption) trace.Tracer {
+	if tp != nil {
+		return tp.Tracer(name, opts...)
+	}
+	return &lazyTracer{name: name, opts: opts}
+}
+
+func InitTracerProvider(ctx context.Context, opts ...Option) error {
 	cfg, err := apply(opts...)
 	if err != nil {
 		return err
@@ -89,7 +127,7 @@ func InitTracer(ctx context.Context, opts ...Option) error {
 		return err
 	}
 
-	tp := sdktrace.NewTracerProvider(
+	tp = sdktrace.NewTracerProvider(
 		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithSyncer(exporter),
 		//		sdktrace.WithBatcher(exporter),
@@ -100,4 +138,10 @@ func InitTracer(ctx context.Context, opts ...Option) error {
 	otel.SetTracerProvider(tp)
 
 	return nil
+}
+
+func Shutdown(ctx context.Context) {
+	if tp != nil {
+		tp.Shutdown(ctx)
+	}
 }
