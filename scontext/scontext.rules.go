d80710feeafd82224cb2d6cda97ae4cb94b75bd0
fkxxyz <fkxxyz@163.com>
2022-12-07 13:11:19 +0800

    feat: debug bridge/sshd switch

--- scontext/scontext.rules.go
+++ scontext/scontext.rules.go
@@ -7,15 +7,15 @@ func ListPermission(methodName string) []int {
 	case "cloud.lazycat.apis.sys.PackageManager.PauseAppDownload":
 		// [INSTALL_PACKAGE]
 		return []int{1}
+	case "cloud.lazycat.apis.common.PeripheralDeviceService.MountDisk":
+		// [USER_DOCUMENT]
+		return []int{0}
 	case "cloud.lazycat.apis.sys.PackageManager.Install":
 		// [INSTALL_PACKAGE]
 		return []int{1}
 	case "cloud.lazycat.apis.sys.PackageManager.Uninstall":
 		// [INSTALL_PACKAGE]
 		return []int{1}
-	case "cloud.lazycat.apis.common.PeripheralDeviceService.MountDisk":
-		// [USER_DOCUMENT]
-		return []int{0}
 	}
 	return nil
 }
