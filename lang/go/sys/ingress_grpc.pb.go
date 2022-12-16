ccf07286f442668ad283196393135bb86ec55ec5
czx <caozhenxiang@linakesi.com>
2022-12-16 11:44:04 +0800

    ingress: make block list into allow list

--- lang/go/sys/ingress_grpc.pb.go
+++ lang/go/sys/ingress_grpc.pb.go
@@ -25,18 +25,18 @@ const _ = grpc.SupportPackageIsVersion7
 type IngressServiceClient interface {
 	GetDevSetting(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*DevSetting, error)
 	SetDevSetting(ctx context.Context, in *DevSetting, opts ...grpc.CallOption) (*DevSetting, error)
-	// 阻止一个用户对指定 app 的访问
-	BlockAdd(ctx context.Context, in *IngressBlockRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
-	// 恢复一个用户对指定 app 的访问权限
-	BlockDel(ctx context.Context, in *IngressBlockRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
-	// 恢复一个用户对所有 app 的访问权限
-	BlockDelAll(ctx context.Context, in *IngressBlockDelAllRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
-	// 查询一个用户是否被阻止访问指定 app
-	BlockGet(ctx context.Context, in *IngressBlockGetRequest, opts ...grpc.CallOption) (*IngressBlockGetResponse, error)
-	// 列出一个用户的所有被阻止访问的 app 列表
-	BlockList(ctx context.Context, in *IngressBlockListRequest, opts ...grpc.CallOption) (*IngressBlockListResponse, error)
-	// 清空黑名单
-	BlockClear(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	// 允许一个用户对指定 app 的访问
+	AllowAdd(ctx context.Context, in *IngressAllowRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	// 阻止一个用户对指定 app 的访问权限
+	AllowDel(ctx context.Context, in *IngressAllowRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	// 阻止一个用户对所有 app 的访问权限
+	AllowDelAll(ctx context.Context, in *IngressAllowDelAllRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+	// 查询一个用户是否被允许访问指定 app
+	AllowGet(ctx context.Context, in *IngressAllowGetRequest, opts ...grpc.CallOption) (*IngressAllowGetResponse, error)
+	// 列出一个用户的所有被允许访问的 app 列表
+	AllowList(ctx context.Context, in *IngressAllowListRequest, opts ...grpc.CallOption) (*IngressAllowListResponse, error)
+	// 清空白名单
+	AllowClear(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error)
 	// 获取指定 app 最后一次被访问的时刻
 	GetAppLastAccessTime(ctx context.Context, in *IngressAppLastAccessTimeRequest, opts ...grpc.CallOption) (*IngressAppLastAccessTimeResponse, error)
 }
@@ -67,54 +67,54 @@ func (c *ingressServiceClient) SetDevSetting(ctx context.Context, in *DevSetting
 	return out, nil
 }
 
-func (c *ingressServiceClient) BlockAdd(ctx context.Context, in *IngressBlockRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+func (c *ingressServiceClient) AllowAdd(ctx context.Context, in *IngressAllowRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/BlockAdd", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/AllowAdd", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *ingressServiceClient) BlockDel(ctx context.Context, in *IngressBlockRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+func (c *ingressServiceClient) AllowDel(ctx context.Context, in *IngressAllowRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/BlockDel", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/AllowDel", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *ingressServiceClient) BlockDelAll(ctx context.Context, in *IngressBlockDelAllRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+func (c *ingressServiceClient) AllowDelAll(ctx context.Context, in *IngressAllowDelAllRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/BlockDelAll", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/AllowDelAll", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *ingressServiceClient) BlockGet(ctx context.Context, in *IngressBlockGetRequest, opts ...grpc.CallOption) (*IngressBlockGetResponse, error) {
-	out := new(IngressBlockGetResponse)
-	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/BlockGet", in, out, opts...)
+func (c *ingressServiceClient) AllowGet(ctx context.Context, in *IngressAllowGetRequest, opts ...grpc.CallOption) (*IngressAllowGetResponse, error) {
+	out := new(IngressAllowGetResponse)
+	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/AllowGet", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *ingressServiceClient) BlockList(ctx context.Context, in *IngressBlockListRequest, opts ...grpc.CallOption) (*IngressBlockListResponse, error) {
-	out := new(IngressBlockListResponse)
-	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/BlockList", in, out, opts...)
+func (c *ingressServiceClient) AllowList(ctx context.Context, in *IngressAllowListRequest, opts ...grpc.CallOption) (*IngressAllowListResponse, error) {
+	out := new(IngressAllowListResponse)
+	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/AllowList", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *ingressServiceClient) BlockClear(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+func (c *ingressServiceClient) AllowClear(ctx context.Context, in *emptypb.Empty, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	out := new(emptypb.Empty)
-	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/BlockClear", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/cloud.lazycat.apis.sys.IngressService/AllowClear", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -136,18 +136,18 @@ func (c *ingressServiceClient) GetAppLastAccessTime(ctx context.Context, in *Ing
 type IngressServiceServer interface {
 	GetDevSetting(context.Context, *emptypb.Empty) (*DevSetting, error)
 	SetDevSetting(context.Context, *DevSetting) (*DevSetting, error)
-	// 阻止一个用户对指定 app 的访问
-	BlockAdd(context.Context, *IngressBlockRequest) (*emptypb.Empty, error)
-	// 恢复一个用户对指定 app 的访问权限
-	BlockDel(context.Context, *IngressBlockRequest) (*emptypb.Empty, error)
-	// 恢复一个用户对所有 app 的访问权限
-	BlockDelAll(context.Context, *IngressBlockDelAllRequest) (*emptypb.Empty, error)
-	// 查询一个用户是否被阻止访问指定 app
-	BlockGet(context.Context, *IngressBlockGetRequest) (*IngressBlockGetResponse, error)
-	// 列出一个用户的所有被阻止访问的 app 列表
-	BlockList(context.Context, *IngressBlockListRequest) (*IngressBlockListResponse, error)
-	// 清空黑名单
-	BlockClear(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
+	// 允许一个用户对指定 app 的访问
+	AllowAdd(context.Context, *IngressAllowRequest) (*emptypb.Empty, error)
+	// 阻止一个用户对指定 app 的访问权限
+	AllowDel(context.Context, *IngressAllowRequest) (*emptypb.Empty, error)
+	// 阻止一个用户对所有 app 的访问权限
+	AllowDelAll(context.Context, *IngressAllowDelAllRequest) (*emptypb.Empty, error)
+	// 查询一个用户是否被允许访问指定 app
+	AllowGet(context.Context, *IngressAllowGetRequest) (*IngressAllowGetResponse, error)
+	// 列出一个用户的所有被允许访问的 app 列表
+	AllowList(context.Context, *IngressAllowListRequest) (*IngressAllowListResponse, error)
+	// 清空白名单
+	AllowClear(context.Context, *emptypb.Empty) (*emptypb.Empty, error)
 	// 获取指定 app 最后一次被访问的时刻
 	GetAppLastAccessTime(context.Context, *IngressAppLastAccessTimeRequest) (*IngressAppLastAccessTimeResponse, error)
 	mustEmbedUnimplementedIngressServiceServer()
@@ -163,23 +163,23 @@ func (UnimplementedIngressServiceServer) GetDevSetting(context.Context, *emptypb
 func (UnimplementedIngressServiceServer) SetDevSetting(context.Context, *DevSetting) (*DevSetting, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SetDevSetting not implemented")
 }
-func (UnimplementedIngressServiceServer) BlockAdd(context.Context, *IngressBlockRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method BlockAdd not implemented")
+func (UnimplementedIngressServiceServer) AllowAdd(context.Context, *IngressAllowRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AllowAdd not implemented")
 }
-func (UnimplementedIngressServiceServer) BlockDel(context.Context, *IngressBlockRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method BlockDel not implemented")
+func (UnimplementedIngressServiceServer) AllowDel(context.Context, *IngressAllowRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AllowDel not implemented")
 }
-func (UnimplementedIngressServiceServer) BlockDelAll(context.Context, *IngressBlockDelAllRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method BlockDelAll not implemented")
+func (UnimplementedIngressServiceServer) AllowDelAll(context.Context, *IngressAllowDelAllRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AllowDelAll not implemented")
 }
-func (UnimplementedIngressServiceServer) BlockGet(context.Context, *IngressBlockGetRequest) (*IngressBlockGetResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method BlockGet not implemented")
+func (UnimplementedIngressServiceServer) AllowGet(context.Context, *IngressAllowGetRequest) (*IngressAllowGetResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AllowGet not implemented")
 }
-func (UnimplementedIngressServiceServer) BlockList(context.Context, *IngressBlockListRequest) (*IngressBlockListResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method BlockList not implemented")
+func (UnimplementedIngressServiceServer) AllowList(context.Context, *IngressAllowListRequest) (*IngressAllowListResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AllowList not implemented")
 }
-func (UnimplementedIngressServiceServer) BlockClear(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method BlockClear not implemented")
+func (UnimplementedIngressServiceServer) AllowClear(context.Context, *emptypb.Empty) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method AllowClear not implemented")
 }
 func (UnimplementedIngressServiceServer) GetAppLastAccessTime(context.Context, *IngressAppLastAccessTimeRequest) (*IngressAppLastAccessTimeResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetAppLastAccessTime not implemented")
@@ -233,110 +233,110 @@ func _IngressService_SetDevSetting_Handler(srv interface{}, ctx context.Context,
 	return interceptor(ctx, in, info, handler)
 }
 
-func _IngressService_BlockAdd_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(IngressBlockRequest)
+func _IngressService_AllowAdd_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IngressAllowRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(IngressServiceServer).BlockAdd(ctx, in)
+		return srv.(IngressServiceServer).AllowAdd(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/cloud.lazycat.apis.sys.IngressService/BlockAdd",
+		FullMethod: "/cloud.lazycat.apis.sys.IngressService/AllowAdd",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IngressServiceServer).BlockAdd(ctx, req.(*IngressBlockRequest))
+		return srv.(IngressServiceServer).AllowAdd(ctx, req.(*IngressAllowRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _IngressService_BlockDel_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(IngressBlockRequest)
+func _IngressService_AllowDel_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IngressAllowRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(IngressServiceServer).BlockDel(ctx, in)
+		return srv.(IngressServiceServer).AllowDel(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/cloud.lazycat.apis.sys.IngressService/BlockDel",
+		FullMethod: "/cloud.lazycat.apis.sys.IngressService/AllowDel",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IngressServiceServer).BlockDel(ctx, req.(*IngressBlockRequest))
+		return srv.(IngressServiceServer).AllowDel(ctx, req.(*IngressAllowRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _IngressService_BlockDelAll_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(IngressBlockDelAllRequest)
+func _IngressService_AllowDelAll_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IngressAllowDelAllRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(IngressServiceServer).BlockDelAll(ctx, in)
+		return srv.(IngressServiceServer).AllowDelAll(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/cloud.lazycat.apis.sys.IngressService/BlockDelAll",
+		FullMethod: "/cloud.lazycat.apis.sys.IngressService/AllowDelAll",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IngressServiceServer).BlockDelAll(ctx, req.(*IngressBlockDelAllRequest))
+		return srv.(IngressServiceServer).AllowDelAll(ctx, req.(*IngressAllowDelAllRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _IngressService_BlockGet_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(IngressBlockGetRequest)
+func _IngressService_AllowGet_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IngressAllowGetRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(IngressServiceServer).BlockGet(ctx, in)
+		return srv.(IngressServiceServer).AllowGet(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/cloud.lazycat.apis.sys.IngressService/BlockGet",
+		FullMethod: "/cloud.lazycat.apis.sys.IngressService/AllowGet",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IngressServiceServer).BlockGet(ctx, req.(*IngressBlockGetRequest))
+		return srv.(IngressServiceServer).AllowGet(ctx, req.(*IngressAllowGetRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _IngressService_BlockList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(IngressBlockListRequest)
+func _IngressService_AllowList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(IngressAllowListRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(IngressServiceServer).BlockList(ctx, in)
+		return srv.(IngressServiceServer).AllowList(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/cloud.lazycat.apis.sys.IngressService/BlockList",
+		FullMethod: "/cloud.lazycat.apis.sys.IngressService/AllowList",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IngressServiceServer).BlockList(ctx, req.(*IngressBlockListRequest))
+		return srv.(IngressServiceServer).AllowList(ctx, req.(*IngressAllowListRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _IngressService_BlockClear_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _IngressService_AllowClear_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(emptypb.Empty)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(IngressServiceServer).BlockClear(ctx, in)
+		return srv.(IngressServiceServer).AllowClear(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/cloud.lazycat.apis.sys.IngressService/BlockClear",
+		FullMethod: "/cloud.lazycat.apis.sys.IngressService/AllowClear",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IngressServiceServer).BlockClear(ctx, req.(*emptypb.Empty))
+		return srv.(IngressServiceServer).AllowClear(ctx, req.(*emptypb.Empty))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -375,28 +375,28 @@ var IngressService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _IngressService_SetDevSetting_Handler,
 		},
 		{
-			MethodName: "BlockAdd",
-			Handler:    _IngressService_BlockAdd_Handler,
+			MethodName: "AllowAdd",
+			Handler:    _IngressService_AllowAdd_Handler,
 		},
 		{
-			MethodName: "BlockDel",
-			Handler:    _IngressService_BlockDel_Handler,
+			MethodName: "AllowDel",
+			Handler:    _IngressService_AllowDel_Handler,
 		},
 		{
-			MethodName: "BlockDelAll",
-			Handler:    _IngressService_BlockDelAll_Handler,
+			MethodName: "AllowDelAll",
+			Handler:    _IngressService_AllowDelAll_Handler,
 		},
 		{
-			MethodName: "BlockGet",
-			Handler:    _IngressService_BlockGet_Handler,
+			MethodName: "AllowGet",
+			Handler:    _IngressService_AllowGet_Handler,
 		},
 		{
-			MethodName: "BlockList",
-			Handler:    _IngressService_BlockList_Handler,
+			MethodName: "AllowList",
+			Handler:    _IngressService_AllowList_Handler,
 		},
 		{
-			MethodName: "BlockClear",
-			Handler:    _IngressService_BlockClear_Handler,
+			MethodName: "AllowClear",
+			Handler:    _IngressService_AllowClear_Handler,
 		},
 		{
 			MethodName: "GetAppLastAccessTime",
