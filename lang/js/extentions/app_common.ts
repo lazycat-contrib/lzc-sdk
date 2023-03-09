4d82ea0df206a80988b56d4f376ff6ad8f853793
jeesk <meetsong@foxmail.com>
2023-03-09 18:48:45 +0800

    android 兼容懒猫云的壳

--- lang/js/extentions/app_common.ts
+++ lang/js/extentions/app_common.ts
@@ -1,5 +1,26 @@
 import LzcAppSdk, { LzcAppPlatformType, LzcAppSdkManage, native } from './base';
 
+
+async function launchNativeApp(appid: string, filepath: string) {
+    const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
+    const openApprequest = {
+        "appid": appid,
+        "filepath": filepath,
+    }
+    var requestStr = JSON.stringify(openApprequest);
+    jsBridge.LaunchNativeApp(requestStr)
+}
+
+async function launchApp(appid: string, url: string) {
+    const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
+    const openApprequest = {
+        "appid": appid,
+        "url": url,
+    }
+    var openAppRequestJsonStr = JSON.stringify(openApprequest);
+    jsBridge.LaunchApp(openAppRequestJsonStr)
+}
+
 class AppCommon extends LzcAppSdkManage {
 
     @native(LzcAppPlatformType.IOS)
@@ -24,18 +45,16 @@ class AppCommon extends LzcAppSdkManage {
         LzcAppPlatformType.Browser
     )
     public static async LaunchApp(url: string, appid: string): Promise<void> {
-        if (!LzcAppSdk.isInApplication()) {
+        if (LzcAppSdk.isInApplication()) {
             // 判断是否在浏览器中，在浏览器中时或者用户设置为在浏览器打开时，将直接使用 window.open 打开页面
-            window.open(url, '_blank')
+            if (LzcAppSdk.isAndroidWebShell()) {
+                await launchApp(appid, url);
+            } else {
+                window.open(url, '_blank')
+            }
         } else {
             if(LzcAppSdk.isAndroidWebShell()){
-                const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
-                const openApprequest = {
-                    "appid": appid,
-                    "url": url,
-                }
-                var openAppRequestJsonStr = JSON.stringify(openApprequest);
-                jsBridge.LaunchApp(openAppRequestJsonStr)
+                await launchApp(appid, url);
             }else{
                 const jsBridge = await LzcAppSdk.useNativeAsync()
                 jsBridge.LaunchApp(url, appid)
@@ -45,16 +64,18 @@ class AppCommon extends LzcAppSdkManage {
 
     public static async LaunchNativeApp(filepath: string,appid: string) {
         // TODO: 待实现
-        if(LzcAppSdk.isAndroidWebShell()){
-            const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
-            const openApprequest = {
-                "appid": appid,
-                "filepath": filepath,
+        if(LzcAppSdk.isInApplication()){
+            if(LzcAppSdk.isAndroidWebShell()){
+                await launchNativeApp(appid, filepath);
+            }else{
+                console.error("LaunchNativeApp 方法暂未实现。")
             }
-            var requestStr = JSON.stringify(openApprequest);
-            jsBridge.LaunchNativeApp(requestStr)
         }else{
-            console.error("LaunchNativeApp 方法暂未实现。")
+            if(LzcAppSdk.isAndroidWebShell()){
+                await launchNativeApp(appid, filepath);
+            }else{
+                console.error("LaunchNativeApp 方法暂未实现。")
+            }
         }
     }
 
