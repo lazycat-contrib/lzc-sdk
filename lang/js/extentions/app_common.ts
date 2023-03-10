f0fdc60bb6cac414cda60bf24bec2b505c1ec6e9
runui <pbk6@PBK6.cn>
2023-03-10 10:17:50 +0800

    fix(lang/js): repair AppCommon LaunchApp

--- lang/js/extentions/app_common.ts
+++ lang/js/extentions/app_common.ts
@@ -1,29 +1,13 @@
 import LzcAppSdk, { LzcAppPlatformType, LzcAppSdkManage, native } from './base';
 
-
-async function launchNativeApp(appid: string, filepath: string) {
-    const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
-    const openApprequest = {
-        "appid": appid,
-        "filepath": filepath,
-    }
-    var requestStr = JSON.stringify(openApprequest);
-    jsBridge.LaunchNativeApp(requestStr)
-}
-
-async function launchApp(appid: string, url: string) {
-    const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
-    const openApprequest = {
-        "appid": appid,
-        "url": url,
-    }
-    var openAppRequestJsonStr = JSON.stringify(openApprequest);
-    jsBridge.LaunchApp(openAppRequestJsonStr)
-}
-
 class AppCommon extends LzcAppSdkManage {
 
     @native(LzcAppPlatformType.IOS)
+    /**
+     * @description: IOS 测试函数
+     * @deprecated: 该函数仅用于 IOS 测试
+     * @return {*}
+     */
     public static async ScriptHandlers(): Promise<string[]> {
         // const callback = await LzcAppSdk.useNative.ScriptHandlers()
         // console.log("LzcAppSdk.useNative", LzcAppSdk.useNative);
@@ -45,38 +29,60 @@ class AppCommon extends LzcAppSdkManage {
         LzcAppPlatformType.Browser
     )
     public static async LaunchApp(url: string, appid: string): Promise<void> {
-        if (LzcAppSdk.isInApplication()) {
-            // 判断是否在浏览器中，在浏览器中时或者用户设置为在浏览器打开时，将直接使用 window.open 打开页面
-            if (LzcAppSdk.isAndroidWebShell()) {
-                await launchApp(appid, url);
-            } else {
-                window.open(url, '_blank')
+        // 判断是否在浏览器中
+        if (!LzcAppSdk.isInApplication()) {
+            window.open(url, '_blank')
+            return
+        }
+
+        // 在 Android 环境中
+        if (LzcAppSdk.isAndroidWebShell()) {
+            const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
+            const openApprequest = {
+                "appid": appid,
+                "url": url,
             }
-        } else {
-            if(LzcAppSdk.isAndroidWebShell()){
-                await launchApp(appid, url);
-            }else{
-                const jsBridge = await LzcAppSdk.useNativeAsync()
-                jsBridge.LaunchApp(url, appid)
-            }
+            var openAppRequestJsonStr = JSON.stringify(openApprequest);
+            jsBridge.LaunchApp(openAppRequestJsonStr)
+            return
+        }
+
+        if (LzcAppSdk.isIosWebShell()) {
+            const jsBridge = await LzcAppSdk.useNativeAsync()
+            jsBridge.LaunchApp(url, appid)
+            return
         }
     }
 
-    public static async LaunchNativeApp(filepath: string,appid: string) {
-        // TODO: 待实现
-        if(LzcAppSdk.isInApplication()){
-            if(LzcAppSdk.isAndroidWebShell()){
-                await launchNativeApp(appid, filepath);
-            }else{
-                console.error("LaunchNativeApp 方法暂未实现。")
-            }
-        }else{
-            if(LzcAppSdk.isAndroidWebShell()){
-                await launchNativeApp(appid, filepath);
-            }else{
-                console.error("LaunchNativeApp 方法暂未实现。")
-            }
+    @native(
+        LzcAppPlatformType.Android,
+    )
+    /**
+     * @description: 打开原生应用
+     * @param {string} filepath
+     * @param {string} appid
+     * @return {*}
+     */
+    public static async LaunchNativeApp(filepath: string, appid: string): Promise<void> {
+        // 在浏览器环境中
+        if (!LzcAppSdk.isInApplication()) {
+            console.error("LaunchNativeApp 方法暂未实现。")
+            return
         }
+
+        // 在 Android 环境
+        if (LzcAppSdk.isAndroidWebShell()) {
+            const jsBridge = await LzcAppSdk.useNativeAsync(android_launch_service)
+            const openApprequest = {
+                "appid": appid,
+                "filepath": filepath,
+            }
+            var requestStr = JSON.stringify(openApprequest);
+            jsBridge.LaunchNativeApp(requestStr)
+        }
+
+        // TODO: 其他环境中待实现
+        console.error("LaunchNativeApp 方法暂未实现。")
     }
 
 }
