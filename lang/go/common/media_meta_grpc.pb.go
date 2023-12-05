724bbd9f801caee3fbae18becb1f85787fa6e858
luhuaei <luhuaei@gmail.com>
2023-12-05 16:01:54 +0800

    feat: update missing pb code

--- lang/go/common/media_meta_grpc.pb.go
+++ lang/go/common/media_meta_grpc.pb.go
@@ -4,6 +4,10 @@
 // - protoc             v4.23.4
 // source: common/media_meta.proto
 
+// lzc-thumbnail 中会通过 /run/lzc-sys/lzc-thumbnail.sock 提供以下服务
+// - MediaMeta 提供视频或者图片的 metadata 信息
+// - Thumbnail 生成视频或者缩略图，目前不支持指定输出的格式和位置。生成的缩略图只可以通过 thumbnail gateway 的方式访问
+
 package common
 
 import (
@@ -11,6 +15,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -175,5 +180,95 @@ var MediaMeta_ServiceDesc = grpc.ServiceDesc{
 			ClientStreams: true,
 		},
 	},
+	Metadata: "common/media_meta.proto",
+}
+
+const (
+	Thumbnail_GenerateFileThumbnail_FullMethodName = "/cloud.lazycat.apis.common.Thumbnail/GenerateFileThumbnail"
+)
+
+// ThumbnailClient is the client API for Thumbnail service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type ThumbnailClient interface {
+	GenerateFileThumbnail(ctx context.Context, in *GenerateFileThumbnailRequest, opts ...grpc.CallOption) (*emptypb.Empty, error)
+}
+
+type thumbnailClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewThumbnailClient(cc grpc.ClientConnInterface) ThumbnailClient {
+	return &thumbnailClient{cc}
+}
+
+func (c *thumbnailClient) GenerateFileThumbnail(ctx context.Context, in *GenerateFileThumbnailRequest, opts ...grpc.CallOption) (*emptypb.Empty, error) {
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, Thumbnail_GenerateFileThumbnail_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// ThumbnailServer is the server API for Thumbnail service.
+// All implementations must embed UnimplementedThumbnailServer
+// for forward compatibility
+type ThumbnailServer interface {
+	GenerateFileThumbnail(context.Context, *GenerateFileThumbnailRequest) (*emptypb.Empty, error)
+	mustEmbedUnimplementedThumbnailServer()
+}
+
+// UnimplementedThumbnailServer must be embedded to have forward compatible implementations.
+type UnimplementedThumbnailServer struct {
+}
+
+func (UnimplementedThumbnailServer) GenerateFileThumbnail(context.Context, *GenerateFileThumbnailRequest) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GenerateFileThumbnail not implemented")
+}
+func (UnimplementedThumbnailServer) mustEmbedUnimplementedThumbnailServer() {}
+
+// UnsafeThumbnailServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to ThumbnailServer will
+// result in compilation errors.
+type UnsafeThumbnailServer interface {
+	mustEmbedUnimplementedThumbnailServer()
+}
+
+func RegisterThumbnailServer(s grpc.ServiceRegistrar, srv ThumbnailServer) {
+	s.RegisterService(&Thumbnail_ServiceDesc, srv)
+}
+
+func _Thumbnail_GenerateFileThumbnail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GenerateFileThumbnailRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ThumbnailServer).GenerateFileThumbnail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: Thumbnail_GenerateFileThumbnail_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ThumbnailServer).GenerateFileThumbnail(ctx, req.(*GenerateFileThumbnailRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// Thumbnail_ServiceDesc is the grpc.ServiceDesc for Thumbnail service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Thumbnail_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "cloud.lazycat.apis.common.Thumbnail",
+	HandlerType: (*ThumbnailServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "GenerateFileThumbnail",
+			Handler:    _Thumbnail_GenerateFileThumbnail_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
 	Metadata: "common/media_meta.proto",
 }
