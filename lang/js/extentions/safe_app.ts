a8666ce31c939ced3d1b7a2f29064fc5df61b299
SongQifu <songqifu@linakesi.com>
2024-01-19 11:21:08 +0800

    chore: 去掉关于键盘的函数

--- lang/js/extentions/safe_app.ts
+++ lang/js/extentions/safe_app.ts
@@ -1,5 +1,5 @@
-import App, { LzcClient } from "./base"
-import LzcAppSdk from "./base"
+import App from "./base"
+import LzcAppSdk, {LzcClient} from "./base"
 
 // android懒猫云商城的第三方应用在打包后，再次激活该窗口的时候，会发送事件。 页面可以通过事件传来的地址，
 
@@ -11,96 +11,90 @@ const SCREEN_ORIENTATION_LANDSCAPE = 2
 const SCREEN_ORIENTATION_PORTRAIT = 3
 
 export const SAFE = {
-  // h5调用webview 打开App
-  OpenApp: function (url, isFullScreen) {
-    if (App.isWebShell()) {
-      return
+    // h5调用webview 打开App
+    OpenApp: function (url, isFullScreen) {
+        if (App.isWebShell()) {
+            return
+        }
+        LzcClient.OpenApp(url, isFullScreen)
+    },
+    // 目前只支持android
+    // 设置指定key 对应的value
+    SetValue: function (key, value) {
+        LzcClient.SetValue(key, value)
+    },
+    // 获取指定key的value
+    // 目前只支持android
+    GetValue: function (key) {
+        return LzcClient.GetValue(key)
+    },
+    // 目前只支持android
+    // 方向请使用常量
+    SetScreenOrientation: function (ori) {
+        LzcClient.SetScreenOrientation(ori)
+    },
+    // 最大255的亮度
+    SetScreentBrightness(number) {
+        LzcClient.SetScreentBrightness(number)
+    },
+    // 获取当前屏幕亮度百分比(0~1)
+    GetScreenBrightNess() {
+        return LzcClient.GetScreenBrightNess()
+    },
+    // 获取当前音量
+    GetCurrentVolume() {
+        return LzcClient.GetCurrentVolume()
+    },
+    // 设置指定音量
+    SetVoice(num) {
+        LzcClient.SetVoice(num)
+    },
+    // 增加音量
+    AddVoice() {
+        LzcClient.AddVoice()
+    },
+    SubVoice() {
+        LzcClient.SubVoice()
+    },
+    CloseWindow() {
+        LzcClient.CloseWindow()
+    },
+    Minimize() {
+        LzcClient.Minimize()
+    },
+    /**
+     * // 仅仅android 实现
+     * @param dimEnable 是否开启dialog遮罩透明
+     * @param backgroundDimAmount 透明度 0~1
+     * @param url dialog显示的地址
+     * @param callback 回调的callback(),接受结果为CloseDialog中的result 参数
+     * @constructor
+     */
+    OpenDialog(dimEnable: boolean, backgroundDimAmount: number, url: string, callback: string) {
+        const openApprequest = {
+            dimEnable: dimEnable,
+            backgroundDimAmount: backgroundDimAmount,
+            url: url,
+        }
+        var openAppRequestJsonStr = JSON.stringify(openApprequest)
+        const jsBridge = LzcAppSdk.useNative(android_dialog)
+        jsBridge.OpenDialog(openAppRequestJsonStr, callback)
+    },
+    /**
+     * 仅仅在打开的dialog中可以使用
+     * @param result 关闭dialog 的回调结果
+     * @constructor
+     */
+    CloseDialog(result: string) {
+        const jsBridge = LzcAppSdk.useNative(android_dialog)
+        jsBridge.Close(result)
+    },
+    InstallLzcClient(url: string) {
+        console.log(lzc_app_manager)
+        const jsBridge = LzcAppSdk.useNative(lzc_app_manager)
+        jsBridge.InstallLzcClient(url)
     }
-    LzcClient.OpenApp(url, isFullScreen)
-  },
-  // 目前只支持android
-  // 设置指定key 对应的value
-  SetValue: function (key, value) {
-    LzcClient.SetValue(key, value)
-  },
-  // 获取指定key的value
-  // 目前只支持android
-  GetValue: function (key) {
-    return LzcClient.GetValue(key)
-  },
-  // 目前只支持android
-  // 方向请使用常量
-  SetScreenOrientation: function (ori) {
-    LzcClient.SetScreenOrientation(ori)
-  },
-  // 最大255的亮度
-  SetScreentBrightness(number) {
-    LzcClient.SetScreentBrightness(number)
-  },
-  // 获取当前屏幕亮度百分比(0~1)
-  GetScreenBrightNess() {
-    return LzcClient.GetScreenBrightNess()
-  },
-  // 获取当前音量
-  GetCurrentVolume() {
-    return LzcClient.GetCurrentVolume()
-  },
-  // 设置指定音量
-  SetVoice(num) {
-    LzcClient.SetVoice(num)
-  },
-  // 增加音量
-  AddVoice() {
-    LzcClient.AddVoice()
-  },
-  SubVoice() {
-    LzcClient.SubVoice()
-  },
-  CloseWindow() {
-    LzcClient.CloseWindow()
-  },
-  Minimize() {
-    LzcClient.Minimize()
-  },
-  /**
-   * // 仅仅android 实现
-   * @param dimEnable 是否开启dialog遮罩透明
-   * @param backgroundDimAmount 透明度 0~1
-   * @param url dialog显示的地址
-   * @param callback 回调的callback(),接受结果为CloseDialog中的result 参数
-   * @constructor
-   */
-  OpenDialog(dimEnable: boolean, backgroundDimAmount: number, url: string, callback: string) {
-    const openApprequest = {
-      dimEnable: dimEnable,
-      backgroundDimAmount: backgroundDimAmount,
-      url: url,
-    }
-    var openAppRequestJsonStr = JSON.stringify(openApprequest)
-    const jsBridge = LzcAppSdk.useNative(android_dialog)
-    jsBridge.OpenDialog(openAppRequestJsonStr, callback)
-  },
-  /**
-   * 仅仅在打开的dialog中可以使用
-   * @param result 关闭dialog 的回调结果
-   * @constructor
-   */
-  CloseDialog(result: string) {
-    const jsBridge = LzcAppSdk.useNative(android_dialog)
-    jsBridge.Close(result)
-  },
-  InstallLzcClient(url: string) {
-    console.log(lzc_app_manager)
-    const jsBridge = LzcAppSdk.useNative(lzc_app_manager)
-    jsBridge.InstallLzcClient(url)
-  },
-  /**
-   * 仅仅android 实现
-   * @param enable 是否开启键盘弹出
-   * 部分页面需要禁止当前页面键盘弹出，比如输入密码的时候.
-   * @constructor
-   */
-  EnableKeyboard(enable: boolean) {
-    LzcClient.EnableKeyboard(enable)
-  }
 }
+
+
+android_launch_service.LaunchApp('{"appid":"com.tencent.m12m:com.tencent.mm.ui.tools.ShareImgUI","webdavUrl":"https://file.lnks009.heiyu.space/_lzc/files/home/Pictures/%E9%A9%AC%E7%9D%A3%E5%B7%A5673.mp4","boxName":"home"}')
