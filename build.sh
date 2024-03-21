d1302a79a3845dfd58162b5dd854de24eed9bbc4
kkocdko <kkocdko@163.com>
2024-03-21 16:00:12 +0800

    feat: git core.filemode = false is not needed now

--- build.sh
+++ build.sh
@@ -95,7 +95,7 @@ if [ "$1" = "gen-security-content-rules" ]; then
         ./common/*.proto
     popd
     # 容器内总是 root，写入的文件 owner = root 导致外部的非 root 用户被 denied，这里修复。可以考虑 git config core.filemode false 避免 git 发疯
-    chmod -R 777 ./lang # 之前的 chown 设置一个不存在的 uid 在 macos 上的 docker 内出错
+    chmod -R a+rw ./lang # 之前的 chown 设置一个不存在的 uid 在 macos 上的 docker 内出错
     exit
 fi
 
