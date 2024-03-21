90c2f5ee4bd4387c70811265eeefd5919b0ddd7b
kkocdko <kkocdko@163.com>
2024-03-22 01:11:46 +0800

    feat: smaller builder image

--- build.sh
+++ build.sh
@@ -133,11 +133,11 @@ if [ "$1" = "build-builder-image" ]; then
     tar -xf nodejs.tar.xz --strip-components 1
     mv lib/node_modules bin/node_modules # force fix path error
     cd ../..
-    if [ ! -e ~/.docker/cli-plugins/docker-buildx ]; then
-        mkdir -p ~/.docker/cli-plugins/
-        curl -o ~/.docker/cli-plugins/docker-buildx -L https://github.com/docker/buildx/releases/download/v0.13.1/buildx-v0.13.1.linux-amd64
-        chmod +x ~/.docker/cli-plugins/docker-buildx
-    fi
+    # if [ ! -e ~/.docker/cli-plugins/docker-buildx ]; then
+    #     mkdir -p ~/.docker/cli-plugins/
+    #     curl -o ~/.docker/cli-plugins/docker-buildx -L https://github.com/docker/buildx/releases/download/v0.13.1/buildx-v0.13.1.linux-amd64
+    #     chmod +x ~/.docker/cli-plugins/docker-buildx
+    # fi
     DOCKER_BUILDKIT=1 docker build -t $IMAGE .
     rm -rf cache
     exit
