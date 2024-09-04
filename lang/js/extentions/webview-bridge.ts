31b4ebeb21402702cb5c27383e81b1388eb24399
evanmeek <the_lty_mail@foxmail.com>
2024-09-04 14:33:54 +0800

    bump: npm version v0.1.391.

--- lang/js/extentions/webview-bridge.ts
+++ lang/js/extentions/webview-bridge.ts
@@ -3,33 +3,36 @@ if (window.lzc_bridge == undefined) {
   window.lzc_bridge = {}
 }
 
-async function warpIosBridge(func: string, argv = "") {
+export async function warpIosBridge(func: string, argv = "") {
+  if (window.lzc_bridge === undefined) {
+    window.lzc_bridge = {}
+  }
   const key = Number(Math.random() * 10000000).toFixed()
   const handlerFuncName = `handler_${func}_${key}`
 
-  window.lzc_bridge[handlerFuncName] = function (v: any) {
-    console.log("v:", v)
-    return v
-  }
+  window.lzc_bridge[handlerFuncName] = (v: any) => v
 
-  const proxyHandler = async (func: Function) => {
-    return new Promise(res => {
-      const proxyFunc = new Proxy(func, {
+  const proxyHandler = (targetFunc: Function) => {
+    return new Promise(resolve => {
+      const proxyFunc = new Proxy(targetFunc, {
         apply: (target, thisArg, argvlist) => {
-          const resp = target.apply(thisArg, argvlist)
-          return res(resp)
+          const resp = Reflect.apply(target, thisArg, argvlist)
+          console.log("resp:", resp)
+          resolve(resp)
+          return resp // 确保原函数的返回值不会丢失
         },
       })
-      func = proxyFunc
+      // 直接替换原始函数为代理后的函数
+      window.lzc_bridge[handlerFuncName] = proxyFunc
     })
   }
 
-  // requst swift code
+  // 发送请求给 Swift 代码
   window.webkit.messageHandlers[func].postMessage({ handler: handlerFuncName, argv: argv })
-  return await proxyHandler(window.lzc_bridge[handlerFuncName]).then(resp => {
+
+  // 监听代理函数的调用
+  return proxyHandler(window.lzc_bridge[handlerFuncName]).then(resp => {
     delete window.lzc_bridge[handlerFuncName]
     return resp
   })
 }
-
-export { warpIosBridge }
